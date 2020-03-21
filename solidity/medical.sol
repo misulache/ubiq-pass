7e1f27b981c659447aae9b45076e51dc2d23ce40
Keno Budde <kenobudde@yahoo.de>
2020-03-21 19:26:48 +0100

    updated smart contract

--- solidity/medical.sol
+++ solidity/medical.sol
@@ -1,81 +1,93 @@
 pragma solidity >= 0.6.1;
 contract medical {
 
-    struct users 
+    struct users
     {
         bytes32 name;
         bool isSick;
         bool feelsSick;
         bool isDoctor;
         bool verified;
+        address trustedDoctor;
     }
-    
+
     address public owner;
-    
+
     modifier onlyOwner()
     {
         require(msg.sender == owner);
         _;
     }
-    
+
     modifier onlyVerifiedDoctors() {
         require(UserStore[msg.sender].verified && UserStore[msg.sender].isDoctor, "not a verified doctor");
         _;
     }
-    
+
     mapping(address => users) public UserStore;
-    
-    constructor() public 
+
+    constructor() public
     {
         owner = msg.sender;
     }
-    
-      function setIsSick (bool _isSick) public onlyVerifiedDoctors
+
+      function setIsSick (bool _isSick, address _sickPatient) public onlyVerifiedDoctors
     {
-       UserStore[msg.sender].isSick = _isSick;
+       require(UserStore[_sickPatient].trustedDoctor == msg.sender, "not a trusted doctor");
+       UserStore[_sickPatient].isSick = _isSick;
     }
-    
+
      function verifyMedical (address _Doctor) public onlyOwner
     {
        UserStore[_Doctor].isDoctor = true;
     }
-    
-     function registerUser(bytes32 _name, bool _isUser) public
+
+     function registerUser(bytes32 _name, bool _isUser, address _trustedDoctor) public
     {
-       UserStore[msg.sender] = users(_name, false, false, _isUser, false);
+       UserStore[msg.sender] = users(_name, false, false, _isUser, false, _trustedDoctor);
     }
-  
+
     function setFeelsSick(bool _feelsSick) public
     {
        UserStore[msg.sender].feelsSick = _feelsSick;
     }
-    
-    function close() public onlyOwner 
-    { 
-    //onlyOwner is custom modifier, "owner" is the owners address
-        selfdestruct(payable(owner));  
+
+    function setTrustedDoctor(address _trustedDoctor) public
+    {
+       UserStore[msg.sender].trustedDoctor = _trustedDoctor;
     }
 
-   function getIsSick() public view returns (bool isSick) 
+    function close() public onlyOwner
+    {
+    //onlyOwner is custom modifier, "owner" is the owners address
+        selfdestruct(payable(owner));
+    }
+
+   function getIsSick() public view returns (bool isSick)
     {
         return UserStore[msg.sender].isSick;
     }
-        
-    function getIsDoctor() public view returns (bool isDoctor) 
+
+    function getIsDoctor() public view returns (bool isDoctor)
     {
         return UserStore[msg.sender].isDoctor;
     }
-    
-    function getName() public view returns (bytes32) 
+
+    function getName() public view returns (bytes32)
     {
         return UserStore[msg.sender].name;
     }
-    
-    function getFeelsSick() public view returns (bool feelsSick) 
+
+    function getTrustedDoctor() public view returns (bytes32)
+    {
+        return UserStore[UserStore[msg.sender].trustedDoctor].name;
+    }
+
+    function getFeelsSick() public view returns (bool feelsSick)
     {
         return UserStore[msg.sender].feelsSick;
     }
-    
+
     function isVerified() public view returns (bool verified)
     {
         return UserStore[msg.sender].verified;
